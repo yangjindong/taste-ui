23f5aaebb33d80045d8de5bcaf0c929ae7ebe36a
Jindong Yang <yangjindong@gmail.com>
2021-04-20 17:12:04 +0800

    topnav样式更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <template>
-  <router-link to="/">Frank</router-link> |
-  <router-link to="xxx">Frank2</router-link>
-  <hr />
   <router-view />
 </template>
 
@@ -24,6 +21,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="topnav">
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
+  <div class="banner">
+    <h1>轱辘UI</h1>
+    <h2>一个厉害的UI框架</h2>
+    <p class="actions">
+      <a href="">Github</a>
+      <a href="">开始</a>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.topnav {
+  background-color: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
