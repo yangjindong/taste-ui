dd59bc0e521fa6f5709203308a555dc20156f7da
Jindong Yang <yangjindong@gmail.com>
2021-04-20 17:35:36 +0800

    Home页面css

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -2,8 +2,8 @@
   <div class="topnav">
     <div class="logo">LOGO</div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li><a href="">菜单1</a></li>
+      <li><a href="">菜单2</a></li>
     </ul>
   </div>
 </template>
