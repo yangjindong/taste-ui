8b7dc6fc45c93ec156ab15c757f587f7dddac646
Jindong Yang <yangjindong@gmail.com>
2021-04-22 15:52:07 +0800

    可以切换了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,21 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{ checked: checked }"><span></span></button>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const checked = ref(false)
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toggle }
+  }
+})
+</script>
+
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -9,7 +24,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background-color: blue;
+  background-color: gray;
   border-radius: $h/2;
   position: relative;
 }
@@ -22,7 +37,10 @@ span {
   background-color: white;
   border-radius: $h2/2;
 }
-button:hover > span {
+button.checked {
+  background-color: blue;
+}
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
 </style>
