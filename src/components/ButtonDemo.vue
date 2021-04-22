a0f9d1b22b244575ed759516f4e4b51e28e936dc
Jindong Yang <yangjindong@gmail.com>
2021-04-22 18:30:30 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Button组件</div>
+  <div>Button示例</div>
+  <h1>示例一</h1>
+  <Button>你好</Button>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from '../lib/Button.vue'
+
+export default defineComponent({
+  name: 'ButtonDemo',
+  components: {
+    Button
+  },
+  setup() {}
+})
+</script>
