d024634c28b0b822dd614ffe227b942e80bd459d
Jindong Yang <yangjindong@gmail.com>
2021-04-22 15:36:44 +0800

    Switch css 1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,4 +1,25 @@
 <template>
-  <div>switch组件1</div>
+  <button><span></span></button>
 </template>
 
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background-color: blue;
+  border-radius: $h/2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background-color: white;
+  border-radius: $h2/2;
+}
+</style>
