c7310141c2b7da8dbc96a14ba51f90c1c6f8f327
Jindong Yang <yangjindong@gmail.com>
2021-04-23 11:30:24 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Dialog组件</div>
+  <h3>示例1</h3>
+  <Dialog />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Dialog from '../lib/Dialog.vue'
+
+export default defineComponent({
+  name: 'DialogDemo',
+  components: { Dialog },
+  setup() {}
+})
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from './Button.vue'
+
+export default defineComponent({
+  components: { Button },
+  setup() {}
+})
+</script>
