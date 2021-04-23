a10cb026a628dba799888251ec1f20b999c0299a
Jindong Yang <yangjindong@gmail.com>
2021-04-23 11:00:10 +0800

    支持disabled属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -39,6 +39,12 @@
       <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
+  <h3>示例4</h3>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用文字按钮</Button>
+  </div>
 </template>
 
 <script lang="ts">
