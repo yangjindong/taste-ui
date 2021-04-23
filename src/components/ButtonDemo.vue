7986fc580969e3767554ed22fd66333e1b715016
Jindong Yang <yangjindong@gmail.com>
2021-04-23 10:42:04 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -21,6 +21,24 @@
     <Button theme="text">普通</Button>
     <Button theme="text" size="small">小小</Button>
   </div>
+  <h3>示例3</h3>
+  <div>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
