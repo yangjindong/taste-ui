df0fcace82f38072c96166c26d0b1d6d20c1346a
Jindong Yang <yangjindong@gmail.com>
2021-04-23 10:28:39 +0800

    size css 2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,9 +6,21 @@
   <Button theme="link">你好</Button>
   <Button theme="text">你好</Button>
   <h3>示例2</h3>
-  <Button>你好</Button>
-  <Button size="small">你好</Button>
-  <Button size="big">你好</Button>
+  <div>
+    <Button size="big">大大</Button>
+    <Button>普通</Button>
+    <Button size="small">小小</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">大大</Button>
+    <Button theme="link">普通</Button>
+    <Button theme="link" size="small">小小</Button>
+  </div>
+  <div>
+    <Button theme="text" size="big">大大</Button>
+    <Button theme="text">普通</Button>
+    <Button theme="text" size="small">小小</Button>
+  </div>
 </template>
 
 <script lang="ts">
