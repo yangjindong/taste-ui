d73a2878cfa56040320dce0725e2981b95399692
Jindong Yang <yangjindong@gmail.com>
2021-04-25 15:37:51 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItems[index] = el
+            if (t === selected) selectedItem = el
           }
         "
         @click="select(t)"
@@ -40,16 +40,14 @@ export default defineComponent({
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement[]>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter((div) => div.classList.contains('selected'))[0]
-      const { width } = result.getBoundingClientRect()
+      const { width } = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = result.getBoundingClientRect()
+      const { left: left2 } = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -74,7 +72,15 @@ export default defineComponent({
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
-    return { defaults, titles, current, select, navItems, indicator, container }
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      selectedItem,
+      indicator,
+      container
+    }
   }
 })
 </script>
