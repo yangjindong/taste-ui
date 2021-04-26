21ec60fb8f96584b3081ac2cd530a9a1604968de
Jindong Yang <yangjindong@gmail.com>
2021-04-26 10:39:52 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,9 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
+import Intro from './views/Intro.vue'
+import Install from './views/Install.vue'
+import GetStarted from './views/GetStarted.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -16,6 +19,9 @@ export const router = createRouter({
       path: '/doc',
       component: Doc,
       children: [
+        { path: 'intro', component: Intro },
+        { path: 'install', component: Install },
+        { path: 'get-started', component: GetStarted },
         { path: '', component: DocDemo },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
---
+++ src/views/Install.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Install</div>
+</template>
