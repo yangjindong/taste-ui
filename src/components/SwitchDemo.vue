4156571e981d8f87369fc0fba779788d617e7025
Jindong Yang <yangjindong@gmail.com>
2021-05-28 15:57:33 +0800

    小优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled</h2>
       <div class="demo-component">
-        <Switch2Demo />
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -38,7 +38,7 @@ console.log(Switch1Demo)
 
 export default defineComponent({
   name: 'SwitchDemo',
-  components: { Switch1Demo, Switch2Demo, Button },
+  components: { Button },
   setup() {
     const bool = ref(true)
     return { bool, Switch1Demo, Switch2Demo }
