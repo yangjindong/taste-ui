431403cb499cad78653137aedf498a3f088ec814
Jindong Yang <yangjindong@gmail.com>
2021-05-29 20:43:51 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="codeVisible = !codeVisible">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-html" v-html="html" />
@@ -33,7 +34,13 @@ export default defineComponent({
       )
     })
     const codeVisible = ref(false)
-    return { Prism, html, codeVisible }
+    const showCode = () => {
+      codeVisible.value = true
+    }
+    const hideCode = () => {
+      codeVisible.value = false
+    }
+    return { Prism, html, codeVisible, showCode, hideCode }
   }
 })
 </script>
