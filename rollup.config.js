8dfbbb48535883388b23652e64d51661a0407937
Jindong Yang <yangjindong@gmail.com>
2021-06-01 14:11:29 +0800

    update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import esbuild from 'rollup-plugin-esbuild'
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
+import postcss from 'rollup-plugin-postcss'
 import dartSass from 'sass'
 import { terser } from 'rollup-plugin-terser'
 
@@ -23,14 +24,21 @@ export default {
   ],
   plugins: [
     vue({
-      include: /\.vue$/
+      include: /\.vue$/,
+      preprocessStyles: true
     }),
-    scss({ include: /\.scss$/, sass: dartSass }),
+    scss({
+      include: /\.scss$/,
+      // prefix: `@import "./src/lib/taste.scss";`,
+      sass: dartSass
+    }),
+    postcss(),
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === 'production',
       target: 'es2015'
     }),
     terser()
-  ]
+  ],
+  external: ['vue']
 }
